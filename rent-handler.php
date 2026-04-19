a08429bdd7877d98f8bf9147c0b18fbaf6f04a4b
ziadbleiji <71529158+ziadbleiji@users.noreply.github.com>
2026-04-19 22:52:44 +0200

    Add files via upload

--- rent-handler.php
+++ rent-handler.php
@@ -4,17 +4,20 @@ require "database/connection.php";
 
 $user_id = $_SESSION['id'] ?? 0;
 
-if ($user_id == 0) {
-    header("Location: /index.php?url=login-form");
+if (!$user_id) {
+    header("Location: index.php?url=login-form");
     exit;
 }
 
 $car_id = $_POST['car_id'];
+$name = $_POST['name'];
 $start = $_POST['start_date'];
 $end = $_POST['end_date'];
 
-mysqli_query($conn, "INSERT INTO rentals (user_id, car_id, start_date, end_date)
-VALUES ('$user_id','$car_id','$start','$end')");
+mysqli_query($conn, "
+INSERT INTO bookings (user_id, car_id, name, start_date, end_date)
+VALUES ('$user_id','$car_id','$name','$start','$end')
+");
 
-header("Location: /index.php?url=account");
+header("Location: index.php?url=account");
 exit;
